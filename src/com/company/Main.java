d0f9afceebc07109925af8d9aa5c9360ecd41f8c
Bendegúz Kálmán <kalmanbendeguz@gmail.com>
2020-03-11 11:58:04 +0100

    proba

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello világ");
+        System.out.println("Hello világ111");
     }
 }
